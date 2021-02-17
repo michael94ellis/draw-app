de73b1c246f17404177563fc923cf97920e7d978
Michaellis <michael94ellis@gmail.com>
2021-02-17 16:03:54 -0500

    Initial commit
    created base for chat feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddroomComponent } from './addroom/addroom.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { LoginComponent } from './login/login.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'roomlist/:nickname', component: RoomlistComponent },
+  { path: 'addroom', component: AddroomComponent },
+  { path: 'chatroom/:nickname/:roomid', component: ChatroomComponent },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';3
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'uIG5vkoyCGg9Y2hSe8409im5DUdGjtEnd3MInBrz',
+  databaseURL: 'https://drawer-7798a-default-rtdb.firebaseio.com'
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'draw-app';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
