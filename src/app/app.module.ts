093e547f2043e3568d9098ecd7311c5a97227020
Michaellis <michael94ellis@gmail.com>
2021-02-23 19:52:59 -0500

    github page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LoginComponent,
     RoomlistComponent,
     AddroomComponent,
-    ChatroomComponent
+    ChatroomComponent,
   ],
   imports: [
     BrowserModule,
